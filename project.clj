34b06774a31d09ed77317c2fb8c243d9cf16da91
Rahul De <rahul080327@gmail.com>
2018-04-26 09:24:10 +0200

    Settle on logback for (hopefully) sane logging

--- project.clj
+++ project.clj
@@ -32,10 +32,7 @@
                  [javax.xml.bind/jaxb-api "2.3.0"]          ;; For Aleph's XML dependency, Java 8 compat
                  [io.netty/netty-all "4.1.24.Final"]        ;; Forced Netty version for Java 9+ compat
                  [javax.activation/activation "1.1.1"]      ;; Java 9+ compat for XML bindings
-                 [log4j "1.2.17" :exclusions [javax.mail/mail
-                                              javax.jms/jms
-                                              com.sun.jdmk/jmxtools
-                                              com.sun.jmx/jmxri]]]
+                 [ch.qos.logback/logback-classic "1.2.3"]]  ;; For sane logging in the 21st century
   :plugins [[lein-ancient "0.6.15"]]
   :java-source-paths ["src/bob/java"]
   :global-vars {*warn-on-reflection* true}
