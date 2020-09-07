1f0a23ba95d88946562f0624c11b2d296131cd93
Rahul De <rahul080327@gmail.com>
2020-09-07 20:27:34 +0200

    [runner] test step with resource and artifact

--- runner/test/runner/pipeline/core_test.clj
+++ runner/test/runner/pipeline/core_test.clj
@@ -112,10 +112,7 @@
     (is (not (p/mount-needed? {:mounted #{"a-resource"}} {:needs_resource "a-resource"})))
     (is (not (p/mount-needed? {:mounted #{"a-resource"}} {})))))
 
-(deftest ^:integration step-executions
-  (testing "reduces upon build failure"
-    (is (reduced? (p/exec-step (f/fail "this is fine") {}))))
-
+(deftest ^:integration successful-step-executions
   (testing "successful simple step execution"
     (d/pull-image "busybox:musl")
     (u/with-system (fn [db _]
@@ -194,4 +191,58 @@
           (is (empty? (:mounted final-state)))
           (is (= 200
                  (:status (http/get "http://localhost:8001/bob_artifact/test/test/a-artifact-run-id/text")))))
-        (p/gc-images "a-artifact-run-id")))))
+        (p/gc-images "a-artifact-run-id")
+        (http/delete "http://localhost:8001/bob_artifact/test/test/a-artifact-run-id/text"))))
+
+  (testing "successful step with resource and artifact execution"
+    (d/pull-image "busybox:musl")
+    (u/with-system
+      (fn [db _]
+        (crux/await-tx db
+                       (crux/submit-tx db
+                                       [[:crux.tx/put
+                                         {:crux.db/id :bob.resource-provider/git
+                                          :url        "http://localhost:8000"}]]))
+
+        (crux/await-tx db
+                       (crux/submit-tx db
+                                       [[:crux.tx/put
+                                         {:crux.db/id :bob.artifact-store/local
+                                          :url        "http://localhost:8001"}]]))
+        (crux/await-tx db
+                       (crux/submit-tx db
+                                       [[:crux.tx/put
+                                         {:crux.db/id :bob.pipeline.test/test
+                                          :group      "test"
+                                          :name       "test"
+                                          :steps      []
+                                          :vars       {}
+                                          :resources  [{:name     "source"
+                                                        :type     "external"
+                                                        :provider "git"
+                                                        :params   {:repo   "https://github.com/bob-cd/bob"
+                                                                   :branch "master"}}]
+                                          :image      "busybox:musl"}]]))
+        (let [initial-state {:image     "busybox:musl"
+                             :mounted   #{}
+                             :run-id    "a-full-run-id"
+                             :db-client db
+                             :env       {}
+                             :group     "test"
+                             :name      "test"}
+              step          {:needs_resource    "source"
+                             :cmd               "ls"
+                             :produces_artifact {:artifact_path  "README.md"
+                                                 :name           "text"
+                                                 :artifact_store "local"}}
+              final-state   (p/exec-step initial-state step)]
+          (is (not (f/failed? final-state)))
+          (is (contains? (:mounted final-state) "source"))
+          (is (= 200
+                 (:status (http/get "http://localhost:8001/bob_artifact/test/test/a-full-run-id/text")))))
+        (p/gc-images "a-full-run-id")
+        (http/delete "http://localhost:8001/bob_artifact/test/test/a-full-run-id/text")))))
+
+(deftest ^:integration failed-step-executions
+  (testing "reduces upon build failure"
+    (is (reduced? (p/exec-step (f/fail "this is fine") {})))))
