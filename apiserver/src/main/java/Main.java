96ab52394cc9410203bdfc7ebf10aca333bec417
Rahul De <rahul080327@gmail.com>
2020-11-29 10:34:26 +0100

    [apiserver] Read env vars more simply, prune deps

--- apiserver/src/main/java/Main.java
+++ apiserver/src/main/java/Main.java
@@ -15,13 +15,7 @@
  * along with Bob. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import crux.api.ICruxAPI;
-import io.vertx.config.ConfigRetriever;
-import io.vertx.config.ConfigRetrieverOptions;
-import io.vertx.config.ConfigStoreOptions;
-import io.vertx.core.Future;
 import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
 import io.vertx.rabbitmq.RabbitMQClient;
 import io.vertx.rabbitmq.RabbitMQOptions;
 import org.slf4j.Logger;
@@ -32,53 +26,58 @@ import java.net.ConnectException;
 public class Main {
     private final static Logger logger = LoggerFactory.getLogger(Main.class.getName());
 
-    public static void main(String[] args) {
-        final var vertx = Vertx.vertx(new VertxOptions().setHAEnabled(true));
-        final var configRetrieverOptions = new ConfigRetrieverOptions()
-            .addStore(new ConfigStoreOptions().setType("env"));
+    private static String getEnv(String key, String def) {
+        final var value = System.getenv(key);
 
-        ConfigRetriever
-            .create(vertx, configRetrieverOptions)
-            .getConfig()
-            .compose(conf -> {
-                final var storageUrl = conf.getString("BOB_STORAGE_URL", "jdbc:postgresql://localhost:5432/bob");
-                final var storageUser = conf.getString("BOB_STORAGE_USER", "bob");
-                final var storagePassword = conf.getString("BOB_STORAGE_PASSWORD", "bob");
+        return value == null ? def : value;
+    }
 
-                final var queueUrl = conf.getString("BOB_QUEUE_URL", "amqp://localhost:5672");
-                final var queueUser = conf.getString("BOB_QUEUE_USER", "guest");
-                final var queuePassword = conf.getString("BOB_QUEUE_PASSWORD", "guest");
+    private static int getEnv(String key, int def) {
+        final var value = System.getenv(key);
 
-                final var apiHost = conf.getString("BOB_API_HOST", "0.0.0.0");
-                final var apiPort = conf.getInteger("BOB_API_PORT", 7777);
-                final var healthCheckFreq = conf.getInteger("BOB_HEALTH_CHECK_FREQ", 5000);
-                final var connectionRetryAttempts = conf.getInteger("BOB_CONNECTION_RETRY_ATTEMPTS", 10);
-                final var connectionRetryDelay = conf.getInteger("BOB_CONNECTION_RETRY_DELAY", 2000);
+        try {
+            return value == null ? def : Integer.parseInt(value);
+        } catch (NumberFormatException _e) {
+            logger.warn("Reading %s with value %s as an int failed. Defaulting to %d.".formatted(key, value, def));
+            return def;
+        }
+    }
 
-                final ICruxAPI node;
-                try {
-                    node = new DB(
-                        storageUrl, storageUser, storagePassword, connectionRetryAttempts, connectionRetryDelay
-                    ).node;
-                } catch (ConnectException e) {
-                    return Future.failedFuture(e);
-                }
+    public static void main(String[] args) throws ConnectException {
+        final var vertx = Vertx.vertx();
 
-                final var queue = RabbitMQClient.create(
-                    vertx,
-                    new RabbitMQOptions()
-                        .setUri(queueUrl)
-                        .setUser(queueUser)
-                        .setPassword(queuePassword)
-                        .setReconnectAttempts(connectionRetryAttempts)
-                        .setReconnectInterval(connectionRetryDelay)
-                );
+        final var storageUrl = getEnv("BOB_STORAGE_URL", "jdbc:postgresql://localhost:5432/bob");
+        final var storageUser = getEnv("BOB_STORAGE_USER", "bob");
+        final var storagePassword = getEnv("BOB_STORAGE_PASSWORD", "bob");
+        final var queueUrl = getEnv("BOB_QUEUE_URL", "amqp://localhost:5672");
+        final var queueUser = getEnv("BOB_QUEUE_USER", "guest");
+        final var queuePassword = getEnv("BOB_QUEUE_PASSWORD", "guest");
+        final var apiHost = getEnv("BOB_API_HOST", "0.0.0.0");
+        final var apiPort = getEnv("BOB_API_PORT", 7777);
+        final var healthCheckFreq = getEnv("BOB_HEALTH_CHECK_FREQ", 5000);
+        final var connectionRetryAttempts = getEnv("BOB_CONNECTION_RETRY_ATTEMPTS", 10);
+        final var connectionRetryDelay = getEnv("BOB_CONNECTION_RETRY_DELAY", 2000);
 
-                return vertx.deployVerticle(new APIServer(
-                    "bob/api.yaml", apiHost, apiPort, queue, node, healthCheckFreq
-                ));
-            })
-            .onSuccess(id -> logger.info("Deployed on verticle: " + id))
+        logger.info("Starting APIServer");
+
+        final var node = new DB(
+            storageUrl, storageUser, storagePassword, connectionRetryAttempts, connectionRetryDelay
+        ).node;
+        final var queue = RabbitMQClient.create(
+            vertx,
+            new RabbitMQOptions()
+                .setUri(queueUrl)
+                .setUser(queueUser)
+                .setPassword(queuePassword)
+                .setReconnectAttempts(connectionRetryAttempts)
+                .setReconnectInterval(connectionRetryDelay)
+        );
+
+        vertx
+            .deployVerticle(new APIServer(
+                "bob/api.yaml", apiHost, apiPort, queue, node, healthCheckFreq
+            ))
+            .onSuccess(id -> logger.info("APIServer started, deployed on verticle: " + id))
             .onFailure(err -> {
                 logger.error("Deployment error: " + err.getMessage());
                 vertx.close();
