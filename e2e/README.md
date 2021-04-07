d17bbfedf2a13bbe493510c2333d5776858be191
Em Grasmeder <emma@gras.org>
2021-04-07 17:59:07 +0200

    Add e2e tests for pipeline workflow (#98)

--- e2e/README.md
+++ e2e/README.md
@@ -24,5 +24,11 @@ and then from your editor, you can connect to a Socket REPL, if you're using [vi
 
 and from then on, you should be able to evaluate individual tests or code as you're used to.
 
+If you want to do anything involving side effects, you'll need to run:
+```
+docker-compose up
+```
+in the e2e folder to access bob
+
 #### Note
 Because this code is using Babashka, there are certain limitations about which namespaces can be imported. The [Babashka docs](https://book.babashka.org/#built-in-namespaces) enumerate the available namespaces to import. 
--- e2e/end-to-end-tests/tests.clj
+++ e2e/end-to-end-tests/tests.clj
@@ -19,16 +19,70 @@
             [clojure.set :as s]
             [cheshire.core :as json]))
 
-(defn sleep-off-side-effects
-  [f]
-  (f)
-  (Thread/sleep 100))
 
-(t/use-fixtures :each sleep-off-side-effects)
+(def bob-url "http://localhost:7777")
+(def default-message "123")
+(def default-image-name "busybox:latest")
+(def default-pipeline
+  {:steps [{:cmd (str "echo " default-message)}]
+   :image default-image-name})
+(def infinite-pipeline
+  {:steps [{:cmd (str "while :; do echo " default-message "; sleep 1; done")}]
+   :image default-image-name})
+(def default-options
+  {:headers {"content-type" "application/json"}
+   :body    (json/generate-string default-pipeline)})
+(def infinite-options
+  {:headers {"content-type" "application/json"}
+   :body    (json/generate-string infinite-pipeline)})
+(def default-provider-name "resource-git")
+(def default-provider-url "http://localhost:8000")
+
+(defn random-uuid
+  []
+  (str (java.util.UUID/randomUUID)))
 
 (defn get-resp-message
   [body]
   (:message (json/parse-string body true)))
+
+(defn pipeline-status
+  [id]
+  (let [{:keys [body]} @(http/get (format "%s/pipelines/status/runs/%s"
+                                          bob-url
+                                          id))
+        message        (get-resp-message body)]
+    message))
+
+(defn pipeline-running?
+  [id]
+  (= "running" (pipeline-status id)))
+
+(defn pipeline-has-status?
+  [id status]
+  (= status (pipeline-status id)))
+
+(defn pipeline-passed?
+  [id]
+  (= "passed" (pipeline-status id)))
+
+(defn wait-until-true
+  [f]
+  (let [response (f)]
+    (when-not response
+      (Thread/sleep 500)
+      (recur f))))
+
+(defn start-pipeline
+  "Creates a pipeline, waits until the side effect is finished, then returns the run id"
+  [pipeline-group pipeline-name]
+  (let [{:keys [body]} @(http/post (format "%s/pipelines/start/groups/%s/names/%s"
+                                           bob-url
+                                           pipeline-group
+                                           pipeline-name))
+        run-id         (get-resp-message body)
+        _              (wait-until-true #(pipeline-running? run-id))]
+    run-id))
 
 (defn run-id?
   [s]
@@ -37,26 +91,66 @@
       (java.util.UUID/fromString)
       (uuid?)))
 
-(def bob-url "http://localhost:7777")
-
-(defn running?
-  [id]
-  (let [{:keys [body]} @(http/get (format "%s/pipelines/status/runs/%s"
+(defn run-id-exists?
+  [pipeline-group pipeline-name run-id]
+  (let [{:keys [body]} @(http/get (format "%s/pipelines/groups/%s/names/%s/runs"
                                           bob-url
-                                          id))
-        message        (get-resp-message body)]
-    (= "running" message)))
+                                          pipeline-group
+                                          pipeline-name))]
+    (some?
+      (:run_id
+        (first (filter
+                 #(= run-id (:run_id %))
+                 (get-resp-message body)))))))
 
-(defn wait-until-complete
-  ([id]
-   (wait-until-complete id 1000))
-  ([id wait]
-   (Thread/sleep wait)
-   (when (running? id)
-     (recur id wait))))
+(defn pipeline-exists?
+  [pgroup pname]
+  (let [{:keys [body]} @(http/get (format "%s/pipelines" bob-url))
+        pipelines      (get-resp-message body)
 
+        pipeline       (filter #(and (= (:group %) pgroup)
+                                     (= (:name %) pname))
+                               pipelines)]
+    (not-empty pipeline)))
 
+(defn create-pipeline
+  "Returns true if pipeline matching group and name exists"
+  [pgroup pname options]
+  (let [{:keys [body status]} @(http/post (format "%s/pipelines/groups/%s/names/%s"
+                                                  bob-url
+                                                  pgroup
+                                                  pname)
+                                          options)
+        _                     (wait-until-true #(pipeline-exists? pgroup pname))]
+    {:body body :status status}))
 
+(defn pipeline-started?
+  "Returns true if a pipeline status is running or finished"
+  [run-id]
+  (or (pipeline-has-status? run-id "running")
+      (pipeline-has-status? run-id "passed")))
+
+(defn get-pipeline-logs
+  [run-id]
+  @(http/get (format "%s/pipelines/logs/runs/%s/offset/%s/lines/%s"
+                     bob-url
+                     run-id
+                     0
+                     100)))
+
+(defn stop-pipeline-run
+  "Stops pipeline, waits until side effect is done, and returns nil"
+  [pgroup pname run-id]
+
+  @(http/post (str bob-url
+                   "/pipelines/stop/groups/"
+                   pgroup
+                   "/names/"
+                   pname
+                   "/runs/"
+                   run-id))
+
+  (wait-until-true #(pipeline-has-status? run-id "stopped")))
 
 
 (t/deftest health-check-test
@@ -72,84 +166,64 @@
       (t/is (= "application/yaml" (:content-type headers))))))
 
 (t/deftest resource-providers-test
-  (let [provider-name "resource-git"
-        provider-url  "http://localhost:8000"]
-    (t/testing "can add resources"
-      (let [options               {:headers {"content-type" "application/json"}
-                                   :body
-                                   (json/generate-string {:url provider-url})}
-            {:keys [body status]}
-            @(http/post (format "%s/resource-providers/%s"
-                                bob-url
-                                provider-name)
-                        options)]
-        (Thread/sleep 500)
-        (t/is (= 200 status))
-        (t/is (= "Ok" (get-resp-message body)))))
+  (t/testing "can add resources"
+    (let [options               {:headers {"content-type" "application/json"}
+                                 :body
+                                 (json/generate-string {:url default-provider-url})}
+          {:keys [body status]}
+          @(http/post (format "%s/resource-providers/%s"
+                              bob-url
+                              default-provider-name)
+                      options)]
+      (Thread/sleep 500)
+      (t/is (= 200 status))
+      (t/is (= "Ok" (get-resp-message body)))))
 
-    (t/testing "can list resource providers"
-      (let [{:keys [body status]} @(http/get (format "%s/resource-providers" bob-url))]
-        (t/is (= 200 status))
-        (t/is (= [{:name provider-name :url provider-url}] (get-resp-message body)))))
+  (t/testing "can list resource providers"
+    (let [{:keys [body status]} @(http/get (format "%s/resource-providers" bob-url))]
+      (t/is (= 200 status))
+      (t/is (= [{:name default-provider-name :url default-provider-url}] (get-resp-message body)))))
 
-    (t/testing "can remove resource providers"
-      (let [options               {:headers {"content-type" "application/json"}}
-            {:keys [body status]} @(http/delete (format "%s/resource-providers/%s"
-                                                        bob-url
-                                                        provider-name)
-                                                options)]
-        (t/is (= 200 status))
-        (t/is (= "Ok" (get-resp-message body)))))))
+  (t/testing "can remove resource providers"
+    (let [options               {:headers {"content-type" "application/json"}}
+          {:keys [body status]} @(http/delete (format "%s/resource-providers/%s"
+                                                      bob-url
+                                                      default-provider-name)
+                                              options)]
+      (t/is (= 200 status))
+      (t/is (= "Ok" (get-resp-message body))))))
 
 (t/deftest pipeline-test
-  (let [image-name     "busybox:latest"
-        message        "123"
-        pipeline       {:steps [{:cmd (str "echo " message)}]
-                        :image image-name}
-        pipeline-group "some-group"
-        pipeline-name  "some-name"]
-    (t/testing "creates a pipeline"
-      (let [options               {:headers {"content-type" "application/json"}
-                                   :body
-                                   (json/generate-string pipeline)}
+  (t/testing "creates a pipeline"
+    (let [pname            (random-uuid)
+          pgroup           (random-uuid)
+          {:keys [status]} (create-pipeline pgroup pname default-options)]
+      (t/is (= 200 status))
+      (t/testing "and can prove the pipeline exists by listing it"
+        (t/is (pipeline-exists? pgroup pname)))))
 
-            {:keys [body status]} @(http/post (format "%s/pipelines/groups/%s/names/%s"
-                                                      bob-url
-                                                      pipeline-group
-                                                      pipeline-name)
-                                              options)]
-        (t/is (= 200 status))))
-    (t/testing "lists pipelines"
-      (Thread/sleep 500)
-      (let [{:keys [body status]} @(http/get (format "%s/pipelines" bob-url))]
+  (t/testing "starts a pipeline"
+    (let [pname  (random-uuid)
+          pgroup (random-uuid)
+          _      (create-pipeline pgroup pname default-options)
+          run-id (start-pipeline pgroup pname)]
+      (t/is (pipeline-started? run-id))))
 
-        (t/is (= 200 status))
-        (t/is (= {:group pipeline-group
-                  :name  pipeline-name
-                  :image image-name
-                  :steps [{:cmd "echo 123"}]}
-                 (first (get-resp-message body))))))
+  (t/testing "gets the logs of a pipeline"
+    (let [pname                 (random-uuid)
+          pgroup                (random-uuid)
+          _                     (create-pipeline pgroup pname default-options)
+          run-id                (start-pipeline pgroup pname)
+          _                     (wait-until-true #(pipeline-has-status? run-id "passed"))
+          {:keys [body status]} (get-pipeline-logs run-id)
+          message               (get-resp-message body)]
+      (t/is (s/subset? (set [default-message]) (set message)))
+      (t/is (= 200 status))))
 
-    (t/testing "starts a pipeline"
-      (let [{:keys [body status]} @(http/post (format "%s/pipelines/start/groups/%s/names/%s"
-                                                      bob-url
-                                                      pipeline-group
-                                                      pipeline-name))]
-        (t/is (= 200 status))
-        (t/is (run-id? (get-resp-message body)))))
-
-    (t/testing "gets the logs of a pipeline"
-      (let [{:keys [body]}        @(http/post (format "%s/pipelines/start/groups/%s/names/%s"
-                                                      bob-url
-                                                      pipeline-group
-                                                      pipeline-name))
-            run-id                (get-resp-message body)
-            _                     (wait-until-complete run-id)
-            _                     (Thread/sleep 100) ;I'm experiencing flakiness without this
-            {:keys [body status]} @(http/get (format "%s/pipelines/logs/runs/%s/offset/%s/lines/%s"
-                                                     bob-url
-                                                     run-id
-                                                     0
-                                                     100))]
-        (t/is (s/subset? (set [message]) (set (get-resp-message body))))
-        (t/is (= 200 status))))))
+  (t/testing "stops a pipeline"
+    (let [pgroup (random-uuid)
+          pname  (random-uuid)
+          _      (create-pipeline pgroup pname infinite-options)
+          run-id (start-pipeline pgroup pname)
+          _      (stop-pipeline-run pgroup pname run-id)]
+      (t/is (= "stopped" (pipeline-status run-id))))))
