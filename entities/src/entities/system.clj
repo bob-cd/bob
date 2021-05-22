febce21a18b462c7cd791accd072e7a73359fa1e
Rahul De <rahul080327@gmail.com>
2021-05-22 19:17:09 +0200

    [entities] extract common system code

--- entities/src/entities/system.clj
+++ entities/src/entities/system.clj
@@ -15,137 +15,36 @@
 
 (ns entities.system
   (:require [com.stuartsierra.component :as component]
-            [environ.core :as env]
-            [crux.api :as crux]
-            [crux.jdbc :as jdbc]
-            [taoensso.timbre :as log]
-            [langohr.core :as rmq]
-            [langohr.channel :as lch]
-            [langohr.queue :as lq]
-            [langohr.exchange :as le]
-            [langohr.consumers :as lc]
-            [failjure.core :as f]
-            [entities.dispatch :as d])
-  (:import [java.net ConnectException]))
+            [system :as sys]
+            [entities.dispatch :as d]))
 
-(defn int-from-env
-  [key default]
-  (try
-    (Integer/parseInt (get env/env key (str default)))
-    (catch Exception _ default)))
+(defn queue-conf
+  [db]
+  {:exchanges     {"bob.direct" {:type    "direct"
+                                 :durable true}}
+   :queues        {"bob.errors"   {:exclusive   false
+                                   :auto-delete false
+                                   :durable     true}
+                   "bob.entities" {:exclusive   false
+                                   :auto-delete false
+                                   :durable     true}}
+   :bindings      {"bob.entities" "bob.direct"}
+   :subscriptions {"bob.entities" (partial d/queue-msg-subscriber (sys/db-client db))}})
 
-(defonce storage-url (:bob-storage-url env/env "jdbc:postgresql://localhost:5432/bob"))
-(defonce storage-user (:bob-storage-user env/env "bob"))
-(defonce storage-password (:bob-storage-password env/env "bob"))
-
-(defonce queue-url (:bob-queue-url env/env "amqp://localhost:5672"))
-(defonce queue-user (:bob-queue-user env/env "guest"))
-(defonce queue-password (:bob-queue-password env/env "guest"))
-
-(defonce connection-retry-attempts (int-from-env :bob-connection-retry-attempts 10))
-(defonce connection-retry-delay (int-from-env :bob-connection-retry-delay 2000))
-
-(defn try-connect
-  ([conn-fn]
-   (try-connect conn-fn connection-retry-attempts))
-  ([conn-fn n]
-   (if (= n 0)
-     (throw (ConnectException. "Cannot connect to system"))
-     (let [res (f/try*
-                 (conn-fn))]
-       (if (f/failed? res)
-         (do
-           (log/warnf "Connection failed with %s, retrying %d" (f/message res) n)
-           (Thread/sleep connection-retry-delay)
-           (recur conn-fn (dec n)))
-         res)))))
-
-(defprotocol IDatabase
-  (db-client [this]))
-
-(defrecord Database
-  [db-url db-user db-password]
-  component/Lifecycle
-  (start [this]
-    (log/info "Connecting to DB")
-    (assoc this
-           :client
-           (try-connect
-             #(crux/start-node {::jdbc/connection-pool {:dialect 'crux.jdbc.psql/->dialect
-                                                        :db-spec {:jdbcUrl  db-url
-                                                                  :user     db-user
-                                                                  :password db-password}}
-                                :crux/tx-log           {:crux/module     `crux.jdbc/->tx-log
-                                                        :connection-pool ::jdbc/connection-pool}
-                                :crux/document-store   {:crux/module     `crux.jdbc/->document-store
-                                                        :connection-pool ::jdbc/connection-pool}}))))
-  (stop [this]
-    (log/info "Disconnecting DB")
-    (.close (:client this))
-    (assoc this :client nil))
-  IDatabase
-  (db-client [this]
-             (:client this)))
-
-(defprotocol IQueue
-  (queue-chan [this]))
-
-(defrecord Queue
-  [database queue-url queue-user queue-password]
-  component/Lifecycle
-  (start [this]
-    (let [conn            (try-connect #(rmq/connect {:uri      queue-url
-                                                      :username queue-user
-                                                      :password queue-password}))
-          chan            (lch/open conn)
-          entities-queue  "bob.entities"
-          direct-exchange "bob.direct"
-          error-queue     "bob.errors"]
-      (log/infof "Connected on channel id: %d" (.getChannelNumber chan))
-      (le/declare chan direct-exchange "direct" {:durable true})
-      (lq/declare chan
-                  entities-queue
-                  {:exclusive   false
-                   :auto-delete false
-                   :durable     true})
-      (lq/declare chan
-                  error-queue
-                  {:exclusive   false
-                   :auto-delete false
-                   :durable     true})
-      (lq/bind chan entities-queue direct-exchange {:routing-key entities-queue})
-      (lc/subscribe chan entities-queue (partial d/queue-msg-subscriber (db-client database)) {:auto-ack true})
-      (log/infof "Subscribed to %s" entities-queue)
-      (assoc this :conn conn :chan chan)))
-  (stop [this]
-    (log/info "Disconnecting queue")
-    (rmq/close (:conn this))
-    (assoc this :conn nil :chan nil))
-  IQueue
-  (queue-chan [this] (:chan this)))
-
-(def system-map
-  (component/system-map
-    :queue    (component/using (map->Queue {:queue-url      queue-url
-                                            :queue-user     queue-user
-                                            :queue-password queue-password})
-                               [:database])
-    :database (map->Database {:db-url      storage-url
-                              :db-user     storage-user
-                              :db-password storage-password})))
-
-(defonce system nil)
+(def system (atom nil))
 
 (defn start
   []
-  (alter-var-root #'system
-                  (constantly (component/start system-map))))
+  (let [db    (component/start (sys/db))
+        queue (component/start
+                (sys/queue (queue-conf db)))]
+    (reset! system [db queue])))
 
 (defn stop
   []
-  (alter-var-root #'system
-                  #(when %
-                     (component/stop %))))
+  (when @system
+    (run! component/stop (reverse @system))
+    (reset! system nil)))
 
 (defn reset
   []
