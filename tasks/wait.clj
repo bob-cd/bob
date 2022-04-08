42e7f1b33cd1fd7fc72545eb0cef59ebfcc44f86
Rahul De <rahul080327@gmail.com>
2022-04-08 18:55:22 +0100

    Pass wait conf map

--- tasks/wait.clj
+++ tasks/wait.clj
@@ -5,15 +5,15 @@
 ; https://opensource.org/licenses/MIT.
 
 (ns wait
-  (:require [org.httpkit.client :as http]
-            [babashka.process :as proc])
+  (:require [org.httpkit.client :as http])
   (:import [java.net Socket URI]))
 
 (defn retry
   [service logic interval backoff retries]
-  (println (format "Waiting %dms for %s"
+  (println (format "Waiting %dms for %s. %d retries remaining."
                    interval
-                   service))
+                   service
+                   retries))
   (Thread/sleep interval)
   (try
     (logic)
@@ -25,12 +25,13 @@
       (retry service logic (+ interval backoff) backoff (dec retries)))))
 
 (defn wait-for
-  [service url]
-  (let [uri   (URI. url)
-        logic (case (.getScheme uri)
-                "http" #(let [{status :status} @(http/get url)]
-                          (when-not (= status 200)
-                            (throw (Exception. "Not ready."))))
-                "tcp"  #(doto (Socket. (.getHost uri) (.getPort uri))
-                          (.close)))]
-    (retry service logic 1000 200 20)))
+  [conf]
+  (doseq [[service url] conf]
+    (let [uri   (URI. url)
+          logic (case (.getScheme uri)
+                  "http" #(let [{status :status} @(http/get url)]
+                            (when-not (= status 200)
+                              (throw (Exception. "Not ready."))))
+                  "tcp"  #(doto (Socket. (.getHost uri) (.getPort uri))
+                            (.close)))]
+      (retry service logic 1000 200 20))))
