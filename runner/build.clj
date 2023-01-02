b4ef5989fb09a70c184bd70b0cd55a5d85099a46
Rahul De <rahul080327@gmail.com>
2023-01-02 09:18:06 +0000

    [system] remove s3 build hacks

--- runner/build.clj
+++ runner/build.clj
@@ -4,10 +4,6 @@
 ; license that can be found in the LICENSE file or at
 ; https://opensource.org/licenses/MIT.
 
-;; see
-;; https://ask.clojure.org/index.php/10905/control-transient-deps-that-compiled-assembled-into-uberjar?show=10913#c10913
-(require 'clojure.tools.deps.alpha.util.s3-transporter)
-
 (ns build
   (:require [clojure.tools.build.api :as b]))
 
