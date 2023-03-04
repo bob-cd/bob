6d1d15a9fb023d85ffb9471f449b9a2b756067fe
Rahul De <rahul080327@gmail.com>
2023-03-04 09:51:17 +0000

    [tasks] simpler engine selection for tasks

--- tasks/tasks.clj
+++ tasks/tasks.clj
@@ -4,9 +4,13 @@
 ; license that can be found in the LICENSE file or at
 ; https://opensource.org/licenses/MIT.
 
-(ns wait
-  (:require [babashka.http-client :as http])
-  (:import [java.net Socket URI]))
+(ns tasks
+  (:require
+   [babashka.fs :as fs]
+   [babashka.http-client :as http]
+   [babashka.process :as process])
+  (:import
+   [java.net Socket URI]))
 
 (defn retry
   [service logic interval backoff retries]
@@ -33,3 +37,20 @@
                   "tcp" #(doto (Socket. (.getHost uri) (.getPort uri))
                            (.close)))]
       (retry service logic 1000 200 20))))
+
+(defn container-engine
+  []
+  (let [engine (->> ["podman" "docker"]
+                    (map fs/which)
+                    (some (fn [e] (when e e))))]
+    (or engine (throw (ex-info "Podman or Docker not found." {:babashka/exit 1})))))
+
+(defn crun
+  ([cmd]
+   (crun cmd {}))
+  ([cmd opts]
+   (process/shell (merge {:out :inherit :in :inherit :err :inherit} opts) (str (container-engine) " " cmd))))
+
+(defn scrun
+  [cmd]
+  (crun cmd {:continue true}))
